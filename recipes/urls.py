8431c7628632f9aa4f52c304d26ef19c155680a9
Nayonara1 <nayonara.g@escolar.ifrn.edu.br>
2023-01-11 09:24:33 -0300

    aula'

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,12 +1,9 @@
-from django.urls import path 
-from recipes.views import home , sobre ,contato
+from django.urls import path
 
-
-#HTTP REQUEST 
-
+from recipes.views import home, sobre, contato
 
 urlpatterns = [
-    path('',home),
-    path('sobre/',sobre),
-    path('conato/',contato),
+    path('', home),
+    path('sobre/', sobre),
+    path('contato/', contato),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -2,14 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-# HTTP RESPONDE 
-
+# HTTP RESPONSE
 
 def home(request):
- return render (request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html', context={'name': 'Nayonara Galvão'})
 
-def  sobre(request):
- return HttpResponse('<h1>Sobre-  Django</h1>')
+def sobre(request):
+    return HttpResponse('<h1>Sobre - Hello Django</h1>')
 
-def contato(request):   
- return HttpResponse('<h1>Contato- Django</h1>')
+def contato(request):
+    return HttpResponse('<h1>Contato - Hello Django</h1>')
